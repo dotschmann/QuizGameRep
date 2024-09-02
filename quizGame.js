f2c8055cad69090be3044f26797e79c2b9047042
dotschmann <ed.afanyede@gmail.com>
2024-09-02 23:22:23 +0200

    final score displayed

--- quizGame.js
+++ quizGame.js
@@ -2,17 +2,17 @@ const quizInfo = [
     {
         ques: "What is the largest planet in our solar system?",
         choices: ["Earth", "Jupiter", "Saturn", "Mars"],
-        Answer: 1
+        answer: 1
     },
     {
         ques: "Who wrote 'Hamlet'?",
         choices: ["Charles Dickens", "Jane Austen", "William Shakespeare", "Mark Twain"],
-        Answer: 2
+        answer: 2
     },
     {
         ques: "What is the capital city of Japan?",
         choices: ["Kyoto", "Osaka", "Nagoya", "Tokyo"],
-        Answer: 3
+        answer: 3
     }
 ];
 
@@ -27,7 +27,8 @@ function displayQuizInfo(indx) {
             ${quizInfo[indx].choices.map((optn, m) => `                 
                 <li>
                     <label>
-                        <input type = "radio" name = "ques${indx}" value = "${m}" ${choiceSelected[indx] === m ? 'checked' : ' '}>
+                        <input type = "radio" name = "ques${indx}" value = "${m}" 
+                        ${choiceSelected[indx] === m ? 'checked' : ' '}>
                         ${optn}
                     </label>
                 </li>
@@ -53,6 +54,7 @@ function updateButtons() {
     } else {
         document.getElementById("nextBtn").style.display = "inline-block";
         document.getElementById("resultBtn").style.display = "none";
+        document.getElementById("score").style.display = "none";
     }
 
 }
@@ -69,6 +71,20 @@ function previousInfo() {
         currentIndx--;
         displayQuizInfo(currentIndx);
     }
+}
+
+function submitQuiz() {
+    let score = 0;
+
+    quizInfo.forEach((question, m) => {
+        if(choiceSelected[m] === question.answer) {
+            score++;
+        }
+    });
+
+    document.getElementById('score').innerText = `You score ${score} out of ${quizInfo.length}`;
+    document.getElementById("resultBtn").style.display = "none";
+    document.getElementById("score").style.display = "inline-block";
 }
 
 
